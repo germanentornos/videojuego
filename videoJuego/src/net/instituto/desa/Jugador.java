4bc2ed303cb183c6a38fd0783d241143b9410d4c
ALU1J <ALU1J@PC303>
2020-03-02 10:17:50 +0100

    Refactorizado hecho.

--- videoJuego/src/net/instituto/desa/Jugador.java
+++ videoJuego/src/net/instituto/desa/Jugador.java
@@ -3,20 +3,30 @@ package net.instituto.desa;
 public class Jugador {
 	int y, x;
 
-	public int getY() {
-		return y;
+	
+	public void subir(int i) {
+		this.y = this.y + i;
+		
+	}
+	
+	public void bajar(int i) {
+		
+		this.y = this.y - i;
+		
 	}
 
-	public void setY(int y) {
-		this.y = y;
+	public void izquierda(int i) {
+		
+		this.x = this.x - i;
+		
+	}
+	
+	public void derecha(int i) {
+		
+		this.x = this.x + i;
+		
 	}
 
-	public int getX() {
-		return x;
-	}
 
-	public void setX(int x) {
-		this.x = x;
-	}
 
 }
